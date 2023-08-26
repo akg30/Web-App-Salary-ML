b357b1bb0a39b3debc12d586027ad3ad620604ed
Anubhav Kumar Gupta <117196999+akg30@users.noreply.github.com>
2023-08-26 07:09:30 +0530

    Add files via upload

---
+++ streamlit_model_salary_experience.py
@@ -0,0 +1,30 @@
+
+
+import numpy as np
+import pickle as pk
+import streamlit as st
+
+loaded_model=pk.load(open('C:\ml model salary/trained_model.sav','rb'))
+
+def salary_prediction(input_data):
+    input_experience_array=np.asarray(input_data)
+    input_experience_array_new=input_experience_array.reshape(1,-1)
+    experience_salary=loaded_model.predict(input_experience_array_new)
+    import math
+    experience_salary_new='Monthly Salary Is {0} Rupees'.format(math.floor(experience_salary))
+    annual_experience_salary='Annual Salary Is {0} Rupees'.format(math.floor(12*experience_salary))
+    return experience_salary_new,annual_experience_salary
+
+def main():
+    st.title('Salary Prediction By Work Experience Using Machine Learning')
+    experience=st.number_input('Enter Work Experience')
+    salary_with_experience=' '
+    if st.button('Click Here To Get Expected Salary From Work Experience'):
+        salary_with_experience=salary_prediction(experience)
+    st.success(salary_with_experience)
+    st.text('Data Analysis Done And Machine Learning Model Deployed By "Anubhav Kumar Gupta"')
+if __name__=='__main__':
+    main()
+
+
+
